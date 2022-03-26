7bda24ffcfc3f1a9fdc756e93fd6ce998c8a6a5d
Athulkrishna <athulalanchery14@gmail.com>
2022-03-26 11:24:56 +0530

    products

--- lib/components/products_gridView.dart
+++ lib/components/products_gridView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Products extends StatefulWidget {
   const Products({Key? key}) : super(key: key);
 
@@ -7,6 +8,26 @@ class Products extends StatefulWidget {
 }
 
 class _ProductsState extends State<Products> {
+  var product_list = [
+    {
+      "name:",
+      "Blazers",
+      "picture:",
+      "images/products/bllazer1.jpeg",
+      "old_price:",
+      '120',
+      "price:" '85',
+    },
+    {
+      "name:",
+      "Red Dress",
+      "picture:",
+      "images/products/dress2.jpeg",
+      "old_price:",
+      '100',
+      "price:" '50',
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return Container();
