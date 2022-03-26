8e794a7ee10fd70dc143c0a3515031697bafd46f
Athulkrishna <athulalanchery14@gmail.com>
2022-03-26 11:41:40 +0530

    update

--- lib/components/products_gridView.dart
+++ lib/components/products_gridView.dart
@@ -30,6 +30,42 @@ class _ProductsState extends State<Products> {
   ];
   @override
   Widget build(BuildContext context) {
+  
+    return GridView.builder(
+      itemCount: product_list.length,
+      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index){
+          return Single_prod(
+            prod_name: product_list[index]['name'],
+            prod_old_price: product_list[index]['old_price'],
+            prod_picture: product_list[index]['picture'],
+            prod_price: product_list[index]['price'],
+          );
+          });
+      
+    
+  }
+}
+
+class Single_prod extends StatelessWidget {
+  final prod_name;
+  final prod_picture;
+  final prod_old_price;
+  final prod_price;
+  
+  const Single_prod({
+    this.prod_name,
+    this.prod_picture,
+    this.prod_old_price,
+    this.prod_price
+    
+    
+   
+
+  @override
+  Widget build(BuildContext context) {
+
     return Container();
   }
 }
