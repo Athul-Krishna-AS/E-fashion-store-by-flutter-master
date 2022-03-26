090a1d7f3d0b32b0c83e15899c6b70aa5e002e88
Athulkrishna <athulalanchery14@gmail.com>
2022-03-26 13:34:14 +0530

    new change

--- lib/components/products_gridView.dart
+++ lib/components/products_gridView.dart
@@ -1,50 +1,40 @@
 import 'package:flutter/material.dart';
 
 class Products extends StatefulWidget {
-  const Products({Key? key}) : super(key: key);
-
   @override
-  _ProductsState createState() => _ProductsState();
+  State<Products> createState() => _ProductsState();
 }
 
 class _ProductsState extends State<Products> {
   var product_list = [
     {
-      "name:",
-      "Blazers",
-      "picture:",
-      "images/products/bllazer1.jpeg",
-      "old_price:",
-      '120',
-      "price:" '85',
+      "name": "Blazers",
+      "picture": "assets/images/products/blazer1.jpeg",
+      "old_price": '120',
+      "price": '85',
     },
     {
-      "name:",
-      "Red Dress",
-      "picture:",
-      "images/products/dress2.jpeg",
-      "old_price:",
-      '100',
-      "price:" '50',
+      "name": "Red Dress",
+      "picture": "assets/images/products/dress2.jpeg",
+      "old_price": 100,
+      "price": 50,
     }
   ];
+
   @override
   Widget build(BuildContext context) {
-  
     return GridView.builder(
-      itemCount: product_list.length,
-      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2),
-        itemBuilder: (BuildContext context, int index){
+        itemCount: product_list.length,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
           return Single_prod(
             prod_name: product_list[index]['name'],
-            prod_old_price: product_list[index]['old_price'],
             prod_picture: product_list[index]['picture'],
             prod_price: product_list[index]['price'],
+            prod_old_price: product_list[index]['old_price'],
           );
-          });
-      
-    
+        });
   }
 }
 
@@ -53,19 +43,51 @@ class Single_prod extends StatelessWidget {
   final prod_picture;
   final prod_old_price;
   final prod_price;
-  
-  const Single_prod({
-    this.prod_name,
-    this.prod_picture,
-    this.prod_old_price,
-    this.prod_price
-    
-    
-   
+
+  Single_prod(
+      {this.prod_name,
+      this.prod_price,
+      this.prod_picture,
+      this.prod_old_price});
 
   @override
   Widget build(BuildContext context) {
-
-    return Container();
+    return Card(
+      child: Hero(
+        tag: prod_name,
+        child: Material(
+          child: InkWell(
+            onTap: () {},
+            child: GridTile(
+              footer: Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: Text(
+                    prod_name,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  title: Text(
+                    "\₹$prod_price",
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.w800),
+                  ),
+                  subtitle: Text(
+                    "\₹$prod_price",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w800,
+                        decoration: TextDecoration.lineThrough),
+                  ),
+                ),
+              ),
+              child: Image.asset(
+                prod_picture,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
